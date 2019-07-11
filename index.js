a79dc5cf0135689a5e60008db6cb0ea6cfefb774
christopher fajardo <christopher.fajardo73@gmail.com>
2019-07-10 20:48:58 -0400

    finishing facebook login

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const { Builder, By, until } = require("selenium-webdriver");
+const { Builder, By, until } = require('selenium-webdriver');
 
 (async function example() {
-  const driver = await new Builder().forBrowser("chrome").build();
+  const driver = await new Builder().forBrowser('chrome').build();
 
   try {
-    await driver.get("https://www.google.com");
-    await driver.findElement(By.name("q")).sendKeys("nehalist");
-    await driver.findElement(By.id("tsf")).submit();
-    await driver.wait(until.elementLocated(By.id("search")));
-    await driver.findElement(By.linkText("nehalist.io")).click();
-    await driver.wait(until.titleIs("nehalist.io"));
+    await driver.get('https://www.google.com');
+    await driver.findElement(By.name('q')).sendKeys('nehalist');
+    await driver.findElement(By.id('tsf')).submit();
+    await driver.wait(until.elementLocated(By.id('search')));
+    await driver.findElement(By.linkText('nehalist.io')).click();
+    await driver.wait(until.titleIs('nehalist.io'));
   } finally {
     await driver.quit();
   }
 })();
+
+// console.log(process.env.DB_USER);
