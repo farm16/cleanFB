31ef7e2ca90236eaed2f4c6c47075f6c9543ba8e
christopher fajardo <christopher.fajardo73@gmail.com>
2019-07-10 20:55:17 -0400

    small change on index js + setting env vars

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cleanFB
-removal of FB posts 
+
+Many of us probably heard about the exposure of peoples Facebook data recently. And as a result I came across an article by (http://www.jaruzel.com/blog/How-I-Erased-5000-Facebook-Comments-and-Likes) where he developed a tool to erase post from facebook, but this was written in Python so I told my self why not in JavaScript since Selenium Client & WebDriver Language Bindings does support JS
+
+set the following variables
+FB_PASS=fb_pass
+FB_EMAIL=fb_email
--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const { Builder, By, until } = require('selenium-webdriver');
   const driver = await new Builder().forBrowser('chrome').build();
 
   try {
-    await driver.get('https://www.google.com');
-    await driver.findElement(By.name('q')).sendKeys('nehalist');
-    await driver.findElement(By.id('tsf')).submit();
-    await driver.wait(until.elementLocated(By.id('search')));
-    await driver.findElement(By.linkText('nehalist.io')).click();
-    await driver.wait(until.titleIs('nehalist.io'));
+    await driver.get('https://www.facebook.com');
+    await driver
+      .findElement(By.name('email'))
+      .sendKeys('ffajardo6969@hotmail.com');
+    await driver.findElement(By.name('pass')).sendKeys(process.env.FB_PASS);
+    await driver.wait(until.elementLocated(By.id('u_0_a'))).click();
+    await driver.sleep(10000); //sleeps for 10 sec
+    await driver.sleep(10000); //sleeps for 10 sec
   } finally {
     await driver.quit();
   }
-})();
-
-// console.log(process.env.DB_USER);
+})(); // await driver.findElement(By.partialLinkText('u_0_a')).click();
+// await driver.wait(until.elementLocated(By.id("search")));
+// await driver.findElement(By.partialLinkText("nehalist.io")).click();
+// await driver.wait(until.titleIs("nehalist.io"));
